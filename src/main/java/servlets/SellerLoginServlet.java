4702ef7227f062c9ca230d5f35879e92f2c5e1c8
Neil <neilamrstrong92@gmail.com>
2026-02-12 03:08:15 +0100

    updated to haggiazo

--- src/main/java/servlets/SellerLoginServlet.java
+++ src/main/java/servlets/SellerLoginServlet.java
@@ -31,7 +31,7 @@ public class SellerLoginServlet extends HttpServlet {
                 RequestDispatcher rd = req.getRequestDispatcher("SellerHome.html");
 
                 rd.include(req, res);
-                pw.println("    <div id=\"topmid\"><h1>Neil's Online <br>Book Store</h1></div>\r\n"
+                pw.println("    <div id=\"topmid\"><h1>Haggiazo Online  <br>Book Store</h1></div>\r\n"
                         + "    <br>\r\n"
                         + "    <table class=\"tab\">\r\n"
                         + "        <tr>\r\n"
